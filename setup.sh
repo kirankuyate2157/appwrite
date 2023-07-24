705f2981e708ea4b79764ed36d105a95f3501241
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-07-24 11:09:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@
 docker run -it --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
-    --env-file .env \
+    --env-file Config.env \
     --entrypoint="install" \
     appwrite/appwrite:1.3.8
